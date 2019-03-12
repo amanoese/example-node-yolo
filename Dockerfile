2c66b2f2314c26dcdbd998fde89c6846a38d336f
Seito Taka <amanoese@gmail.com>
2019-03-12 17:21:19 +0900

    fix code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,17 @@
-FROM ww24/deep-learning
+FROM nvidia/cuda:10.1-cudnn7-devel-ubuntu16.04
 
 MAINTAINER amanoese
+
+# upgrade packages and install dependent packages
+RUN apt-get update && apt-get upgrade -y && \
+    apt-get install -y curl wget git vim-nox nano build-essential libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-dev libatlas-base-dev python-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+
+# set env
+ENV CPATH=/usr/local/cuda/include:$CPATH
+ENV LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs:$LIBRARY_PATH
+ENV LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+ENV PATH=$GOPATH/bin:/usr/local/cuda/bin:/root/caffe/build/tools:$PATH
+ENV PYTHONPATH=/root/caffe/python:$PYTHONPATH
 
 # nodebrew + nodejs latest
 RUN curl -L git.io/nodebrew | perl - setup
@@ -17,23 +28,18 @@ RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
 
 RUN apt-get update && apt-get install yarn -y
 
-#compile darknet
-RUN git clone https://github.com/OrKoN/darknet /var/darknet
-WORKDIR /var/darknet
-RUN sed -i -e 's/GPU=./GPU=1/' -e 's/OPENCV=./OPENCV=1/' -e 's/OPENMP=./OPENMP=1/' Makefile
-RUN make && make OPENCV=1 OPENMP=1 && make install
-
 # install yolo-node
-RUN . $HOME/.bashrc && yarn global add @moovel/yolo
+#RUN . $HOME/.bashrc && yarn global add @moovel/yolo
 
 # install ffmpeg
-WORKDIR /tmp
+#WORKDIR /tmp
 
-RUN apt-get -y install software-properties-common \
-    && add-apt-repository ppa:mc3man/trusty-media \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends ffmpeg
+#RUN apt-get -y install software-properties-common \
+#    && add-apt-repository ppa:mc3man/trusty-media \
+#    && apt-get update \
+#    && apt-get install -y --no-install-recommends ffmpeg
 
 WORKDIR /var/app
 
-ENTRYPOINT . $HOME/.bashrc && node index.js
+#ENTRYPOINT . $HOME/.bashrc && node index.js
+ENTRYPOINT . $HOME/.bashrc && bash
--- index.js
+++ index.js
@@ -1,51 +1,36 @@
-const darknet = require('@moovel/yolo');
+// import { Darknet } from 'darknet';
+const { Darknet } = require('darknet');
 const fs = require('fs')
-
-darknet.detectImage({
-  cfg: './cfg/yolo.cfg',
-  weights: './yolo.weights',
-  data: 'cfg/coco.data',
-  image:'img_01.jpg',
-  thresh: 0.24, // optional, default: 0.24
-  hierThresh: 0.5, // optional, default: 0.5,
-}, function(modified, original, detections, dimensions) {
-  /**
-
-  modified - raw frame with detections drawn, rgb24 format
-  original - raw frame, as captured by the webcam/video, rgb24 format,
-  detections - array of detections
-  dimenstions - image width and height
-
-  Example detections:
-
-  [ { x: 0.8602103590965271,
-      y: 0.20008485019207,
-      w: 0.13895535469055176,
-      h: 0.39782464504241943,
-      prob: 0.2408987432718277,
-      name: 'tvmonitor' },
-    { x: 0.26072466373443604,
-      y: 0.4977818727493286,
-      w: 0.10842404514551163,
-      h: 0.22796104848384857,
-      prob: 0.3290732204914093,
-      name: 'person' },
-    { x: 0.2568981349468231,
-      y: 0.5765896439552307,
-      w: 0.12322483211755753,
-      h: 0.2544059157371521,
-      prob: 0.2738085687160492,
-      name: 'chair' },
-    { x: 0.6593853235244751,
-      y: 0.8188746571540833,
-      w: 0.06210440397262573,
-      h: 0.100614033639431,
-      prob: 0.3225017189979553,
-      name: 'clock' } ]
-  */
-  console.log({ modified })
-  console.log({ original })
-  console.log({ detections })
-  console.log({ dimensions })
-  fs.writeFileSync("data.raw",modified)
+const Canvas = require('canvas')
+ 
+console.log(__dirname)
+// Init
+let darknet = new Darknet({
+    weights: __dirname + '/yolov3.weights',
+    config: __dirname + '/yolov3.cfg',
+    namefile: __dirname + '/coco.names'
 });
+
+let filename = __dirname + '/img_01.jpg'
+console.log(darknet.detect(filename));
+
+let data = fs.readFileSync(filename)
+
+// データをcanvasのcontextに設定
+var img = new Canvas.Image();
+img.src = data;
+
+var canvas = Canvas.createCanvas(img.width, img.height);
+var ctx = canvas.getContext('2d');
+ctx.drawImage(img, 0, 0, img.width, img.height);
+
+darknet.detect(filename).map(({ name, box : {x,y,w,h} })=>{
+ ctx.strokeStyle = "rgb(200, 0, 0)";
+ ctx.strokeRect(x,y,w,h);
+ ctx.fillText(name,x,y)
+})
+
+const out = fs.createWriteStream(__dirname + '/out.png')
+const stream = canvas.createPNGStream()
+stream.pipe(out)
+out.on('finish', () =>  console.log('The PNG file was created.'))
