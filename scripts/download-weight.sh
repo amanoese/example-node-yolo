8c4b61c1bb42ee13dee3a0c67e7c1b908246888a
Seito Taka <amanoese@gmail.com>
2019-03-14 02:32:58 +0000

    refactoring scripts

--- scripts/download-weight.sh
+++ scripts/download-weight.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-cd $(dirname $0)
+set -eux
+
+cd $(dirname $0)/..
+
+mkdir -p data
+cd data
 
 wget -nc https://pjreddie.com/media/files/yolov3.weights
 wget -nc https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
 wget -nc https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
 
+wget -nc https://github.com/pjreddie/darknet/raw/master/data/dog.jpg img.jpg
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+set -eux
+
+cd $(dirname $0)/..
+./script/download-weight.sh
+
 docker run -it -v$PWD:/var/app node-yolo bash -c 'npm install && node index.js'
